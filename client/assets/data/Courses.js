53852a9e138e3eb647b5e7ed7abecb9011fccf45
JaysaKeylla <43121302+JaysaKeylla@users.noreply.github.com>
2020-10-27 20:47:12 -0300

    Update Courses.js

--- client/assets/data/Courses.js
+++ client/assets/data/Courses.js
@@ -1,23 +1,23 @@
 export const Courses = [
   {
     courseImage: require(`../../assets/img/course-image.jpg`),
-    title: 'GEC 117',
-    description: 'Intro to Technical Drawing',
+    title: 'IMD0001',
+    description: 'Artificial Intelligence',
     lecturer1: require(`../../assets/img/profile-pic.jpg`),
     lecturer2: require(`../../assets/img/profile-pic-4.jpeg`),
     lecturer3: require(`../../assets/img/profile-pic-3.jpeg`),
   },
   {
     courseImage: require(`../../assets/img/course-image.jpg`),
-    title: 'GEC 212',
-    description: 'Intermediate to Technical Drawing',
+    title: 'IMD0002',
+    description: 'Machine Learning',
     lecturer1: require(`../../assets/img/profile-pic.jpg`),
     lecturer2: require(`../../assets/img/profile-pic-4.jpeg`),
     lecturer3: require(`../../assets/img/profile-pic-3.jpeg`),
   },
   {
     courseImage: require(`../../assets/img/course-image.jpg`),
-    title: 'GEC Nothing',
+    title: 'IMD Nothing',
     description: 'Advanced waste fo time',
     lecturer1: require(`../../assets/img/profile-pic.jpg`),
     lecturer2: require(`../../assets/img/profile-pic-4.jpeg`),
